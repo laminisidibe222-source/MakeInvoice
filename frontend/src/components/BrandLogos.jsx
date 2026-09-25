e55bf360f97146d3d5c6e0f396b702d4c3e21c2e
laminisidibe222 <laminisidibe222@gmail.com>
2026-09-25 07:11:12 +0000

    Rename wave logo, fix imports

--- frontend/src/components/BrandLogos.jsx
+++ frontend/src/components/BrandLogos.jsx
@@ -1,4 +1,4 @@
-import waveLogo from '../assets/logos/wave.png';
+import waveLogo from '../assets/logos/wave-logo.png';
 import orangeMoneyLogo from '../assets/logos/orange-money.png';
 import mixxByYasLogo from '../assets/logos/mixxyass.png';
 import cardLogo from '../assets/logos/card.png';
